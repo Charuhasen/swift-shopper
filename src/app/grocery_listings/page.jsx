4533ae1559d89fa393364d92274d8f578f16e1d7
Charuhasen <charuhasen@gmail.com>
2024-01-01 21:47:29 +0000

    user can now categorize their shopping list

--- src/app/grocery_listings/page.jsx
+++ src/app/grocery_listings/page.jsx
@@ -1,19 +1,33 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import organizeGroceryItems from "../../utils/openai";
+import Category from "@/components/category";
 
 export default function GroceryListing() {
-    const [groceryList, setGroceryList] = useState([]);
+    const [groceryList, setGroceryList] = useState({});
 
     useEffect(() => {
         const storedDataString = localStorage.getItem("GroceryList");
         let storedData = JSON.parse(storedDataString);
-        organizeGroceryItems(storedData.groceryList);
+
+        // Assuming organizeGroceryItems is an async function
+        organizeGroceryItems(storedData.groceryList).then((organizedData) => {
+            setGroceryList(organizedData);
+        });
     }, []);
 
     return (
-        <div>
-            <p></p>
+        <div className="w-full flex justify-center items-center flex-col min-h-screen">
+            {console.log(
+                "Page trying to render: " + JSON.stringify(groceryList)
+            )}
+            {Object.keys(groceryList).map((categoryName) => (
+                <Category
+                    key={categoryName}
+                    categoryName={categoryName}
+                    categoryItems={groceryList[categoryName]}
+                />
+            ))}
         </div>
     );
 }
